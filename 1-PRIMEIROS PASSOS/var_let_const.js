6f9a9a702f21dbe56d712667956c72c36307428a
Lucas <lucasalvesgnecco@gmail.com>
2026-06-02 19:19:25 -0300

    variaveis mutaveis e imutaveis

---
+++ 1-PRIMEIROS PASSOS/var_let_const.js
@@ -0,0 +1,25 @@
+var nome = 'joão';
+console.log(nome);
+var idade = 30;
+console.log(idade);
+var nome = 'maria';
+console.log(nome);
+
+let idade_let = 25;
+console.log(idade_let);
+idade_let = 35;
+console.log(idade_let);
+
+const URL = "https://www.google.com.br/"
+console.log(URL);
+const URL_BASE_API = "https://api.exemplo.com/v1";
+const CHAVE_API = "ABCDEFGHIJK";
+console.log("Conectando à API em:", URL_BASE_API);
+console.log("Usando a chave de API:", CHAVE_API);
+
+const MAX_USUARIO_POR_SALA = 10;
+const MODO_DEBUG = false;
+console.log("Máximo de usuários por sala:", MAX_USUARIO_POR_SALA);
+console.log("Modo de depuração ativado:", MODO_DEBUG);
+
+
