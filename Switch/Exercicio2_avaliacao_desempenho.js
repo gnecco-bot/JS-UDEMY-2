68b4c4b4dd75b3b27849dcebd73f9119925361ba
Lucas <lucasalvesgnecco@gmail.com>
2026-06-04 14:13:14 -0300

    exercicio avaliacao desempenho alunos

---
+++ Switch/Exercicio2_avaliacao_desempenho.js
@@ -0,0 +1,37 @@
+const readline = require('readline');
+
+rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+})
+
+rl.question('Digite uma nota de 0 a 100: ', (numero) => {
+    numeroInt = parseInt(numero);
+    let nota;
+
+    const categoria = Math.floor(numeroInt / 10);
+
+    switch (numeroInt) {
+        case 10:
+        case 9:
+            nota = 'A';
+            break;
+
+        case 8:
+            nota = 'B'
+            break;
+
+        case 7:
+            nota = 'C';
+            break;
+
+        case 6:
+            nota = 'D';
+            break;
+
+        default:
+            nota = 'F'
+    }
+    console.log('Sua nota é ' + nota);
+    rl.close()
+});
