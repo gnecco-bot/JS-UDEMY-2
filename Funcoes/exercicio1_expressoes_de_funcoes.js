88ed0148b88667b441e32cac22fef7d14b832f9b
Lucas <lucasalvesgnecco@gmail.com>
2026-06-06 14:33:15 -0300

    exercicio media

---
+++ Funcoes/exercicio1_expressoes_de_funcoes.js
@@ -0,0 +1,9 @@
+const media = function (a, b, c) {
+    return (a + b + c) / 3;
+};
+
+resultado1 = media(2, 6, 7);
+resultado2 = media(20, 67, 44);
+
+console.log(resultado1)
+console.log(resultado2)
