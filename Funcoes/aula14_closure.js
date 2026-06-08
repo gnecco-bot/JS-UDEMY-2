8864e4689ead19c897d172e5223278309523cfc7
Lucas <lucasalvesgnecco@gmail.com>
2026-06-08 20:33:59 -0300

    closure

---
+++ Funcoes/aula14_closure.js
@@ -0,0 +1,34 @@
+function criarContador() {
+    let count = 0;
+    return {
+        incrementar: function () {
+            count += 1;
+            console.log(`Contagem atual: ${count}`);
+        },
+        mostrar: function () {
+            console.log(`Contagem ${count}`);
+        }
+    };
+}
+
+const meuContador = criarContador();
+
+meuContador.mostrar();
+meuContador.incrementar();
+meuContador.incrementar();
+meuContador.incrementar();
+meuContador.mostrar();
+
+function criaAplicadorDeDesconto(valorDesconto) {
+    return function (preco) {
+        const desconto = preco * valorDesconto;
+        const precoFinal = preco - desconto;
+        return precoFinal;
+    };
+}
+
+const aplicarDesconto20 = criaAplicadorDeDesconto(0.20);
+const aplicarDesconto10 = criaAplicadorDeDesconto(0.10);
+
+console.log(aplicarDesconto20(100));
+console.log(aplicarDesconto10(50));
