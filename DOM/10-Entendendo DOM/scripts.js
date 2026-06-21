f58a68880f87c8389bc6307fcc25e838b9bc3b25
Lucas <lucasalvesgnecco@gmail.com>
2026-06-21 13:52:55 -0300

    exemplos de botoes para mudar uma boxcolor

---
+++ DOM/10-Entendendo DOM/scripts.js
@@ -0,0 +1,9 @@
+document.getElementById('mudarCor').addEventListener('click', function () {
+    var box = document.getElementById('colorBox');
+    box.style.backgroundColor = 'salmon';
+});
+
+document.getElementById('redefinirColor').addEventListener('click', function () {
+    var box = document.getElementById('colorBox');
+    box.style.backgroundColor = 'lightblue';
+});
