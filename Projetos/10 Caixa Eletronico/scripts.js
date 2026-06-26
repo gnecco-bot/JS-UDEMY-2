68475bc32efe29e77a7232cf06dc7b6e6213a359
Lucas <lucasalvesgnecco@gmail.com>
2026-06-26 20:08:00 -0300

    Caixa Eletronico

--- Projetos/10 Caixa Eletronico/scripts.js
+++ Projetos/10 Caixa Eletronico/scripts.js
@@ -26,6 +26,7 @@ function formatarSaldo(saldo) {
 };
 
 function logout() {
+    ocultarTelas();
     document.getElementById('login-container').style.display = 'block';
     document.getElementById('atm-container').style.display = 'none';
     document.getElementById('usuario').value = '';
@@ -33,7 +34,68 @@ function logout() {
 }
 
 function depositar() {
-    var usaurio = document.getElementById('usuario').value;
+    var usuario = document.getElementById('usuario').value;
     var valor = parseFloat(document.getElementById('valor-deposito').value);
-
+    var dadosUsuario = JSON.parse(localStorage.getItem(usuario));
+    if (valor > 0 && !isNaN(valor)) {
+        dadosUsuario.saldo += valor;
+        dadosUsuario.historico.push({ tipo: 'Deposito', valor: valor });
+        localStorage.setItem(usuario, JSON.stringify(dadosUsuario))
+        document.getElementById('saldo').innerHTML = formatarSaldo(dadosUsuario.saldo);
+        alert('Depósito realizado com sucesso!');
+        exibirCaixaEletronico()
+    } else {
+        alert('Valor inválido.');
+    }
 }
+
+function exibirDeposito() {
+    ocultarTelas();
+    document.getElementById('deposito-container').style.display = 'block';
+}
+
+function ocultarTelas() {
+    document.getElementById('deposito-container').style.display = 'none';
+    document.getElementById('saque-container').style.display = 'none';
+    document.getElementById('historico-container').style.display = 'none';
+}
+
+function exibirCaixaEletronico() {
+    ocultarTelas();
+    document.getElementById('atm-container').style.display = 'block';
+}
+
+function exibirHistorico() {
+    ocultarTelas();
+    var usuario = document.getElementById('usuario').value;
+    var dadosUsuario = JSON.parse(localStorage.getItem(usuario));
+    var historico = dadosUsuario.historico;
+    var historicoString = '';
+    historico.forEach(function (transacao) {
+        historicoString += transacao.tipo + ': R$ ' + formatarSaldo(transacao.valor) + '\n';
+    });
+    document.getElementById('historico-text').value = historicoString;
+    document.getElementById('historico-container').style.display = 'block';
+    console.log('cheguei aqui')
+};
+
+function exibirSaque() {
+    ocultarTelas();
+    document.getElementById('saque-container').style.display = 'block';
+};
+
+function sacar() {
+    var usuario = document.getElementById('usuario').value;
+    var valor = parseFloat(document.getElementById('valor-saque').value);
+    var dadosUsuario = JSON.parse(localStorage.getItem(usuario));
+    if (valor > 0 && !isNaN(valor) && valor <= dadosUsuario.saldo) {
+        dadosUsuario.saldo -= valor;
+        dadosUsuario.historico.push({ tipo: 'Saque', valor: valor });
+        localStorage.setItem(usuario, JSON.stringify(dadosUsuario));
+        document.getElementById('saldo').innerHTML = formatarSaldo(dadosUsuario.saldo);
+        alert('Saque realizado com sucesso!')
+        exibirCaixaEletronico();
+    } else {
+        alert('Valor inválido ou saldo insuficiente.');
+    };
+};
