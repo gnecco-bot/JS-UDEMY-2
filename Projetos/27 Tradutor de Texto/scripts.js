83c70b8bb1e64f4586c90da15c092772afa6d541
Lucas <lucasalvesgnecco@gmail.com>
2026-07-02 20:52:23 -0300

    Tradutor de Texto

---
+++ Projetos/27 Tradutor de Texto/scripts.js
@@ -0,0 +1,12 @@
+document.getElementById('btnTraduzir').addEventListener('click', function () {
+    const textoOrigem = document.getElementById('textoOrigem').value;
+    const idiomaOrigem = document.getElementById('idiomaOrigem').value;
+    const idiomaDestino = document.getElementById('idiomaDestino').value;
+    fetch(`https://translate.googleapis.com/translate_a/single?client=gtx&sl=${idiomaOrigem}&tl=${idiomaDestino}&dt=t&q=${textoOrigem}`)
+        .then(response => response.json())
+        .then(data => {
+            const traducao = data[0][0][0];
+            document.getElementById('resultado').innerHTML = traducao;
+        })
+        .catch(erro => console.error('Erro ao traduzir texto', erro));
+});
