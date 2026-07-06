7b91debdef67716cd2e9b9ab9d4c2394bbeb109c
Lucas <lucasalvesgnecco@gmail.com>
2026-07-06 19:53:37 -0300

    Scrollers de Textos animados (letreiros)

--- Projetos/35 Scroller de Texto/scripts.js
+++ Projetos/35 Scroller de Texto/scripts.js
@@ -1,3 +1,14 @@
 function abrirPagina(pagina) {
     window.location.href = pagina;
 }
+document.addEventListener('DOMContentLoaded', function () {
+    const scrollers = document.querySelectorAll('.conteudo-led');
+    scrollers.forEach(conteudoTexto => {
+        conteudoTexto.addEventListener('mouseover', function () {
+            this.style.animationPlayState = 'paused';
+        });
+        conteudoTexto.addEventListener('mouseout', function () {
+            this.style.animationPlayState = 'running';
+        });
+    });
+});
