32d2bc54d5e6b8515fee08bce87c660061cf928e
Lucas <lucasalvesgnecco@gmail.com>
2026-07-08 21:34:41 -0300

    Parallax de Frutas

---
+++ Projetos/41 Parallax Frutas/scripts.js
@@ -0,0 +1,7 @@
+// document.addEventListener('scroll', function () {
+//     const elementoParallax = document.querySelectorAll('.parallax');
+//     elementoParallax.forEach(function (elemento) {
+//         let deslocamento = window.pageXOffset;
+//         elemento.style.backgroundPositionX = deslocamento * 0.7 + "px";
+//     });
+// });
